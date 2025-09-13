78b7963254870b965ef4732e255c94bbc672c253
Jorge Rimblas <jorge@rimblas.com>
2025-09-12 19:11:30 -0500

    Previous Votes summary

--- views/wmg_course_vote_summary_v.sql
+++ views/wmg_course_vote_summary_v.sql
@@ -1,15 +1,16 @@
 create or replace view wmg_course_vote_summary_v
 as
 select case 
-when vote is null then 'Total' 
-when vote = -1 then 'Down' 
-when vote = 1 then 'Up'
-when vote = 0 then 'Withhold'
-else to_char(vote)
-end as vote_type
+    when vote is null then 'Total' 
+    when vote = -1 then 'Down' 
+    when vote = 1 then 'Up'
+    when vote = 0 then 'Withhold'
+    else to_char(vote)
+  end as vote_type
 , votes
 from (
   select vote, count(*) votes
+       , count(distinct player_id) players
   from wmg_course_vote_back
   group by rollup(vote)
 )
